b77195dda432db76abb4beb9f227f5c3685ef4eb
chand1012 <chandler@multisiglabs.org>
2023-06-17 17:36:55 -0400

    Remove unused code

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import UsersTable from "@/components/UsersTable";
 import Head from "next/head";
-import React, { useEffect } from "react";
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import React from "react";
 
 import { api } from "@/utils/api";
 
 export default function Users() {
-  const router = useRouter();
-  const { data: session } = useSession();
   const { data, refetch } = api.users.getUsers.useQuery({});
 
   // POST to /api/users/makeAdmin
